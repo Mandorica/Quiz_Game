16fefe6d9fc1474969f8a1597e79888229de2666
flute-piyak <001025shy@gmail.com>
2020-08-23 20:10:16 +0900

    ㅎㅎ
    와!

---
+++ Assets/NameTransfer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+
+
+public class NameTransfer : MonoBehaviour
+{
+
+    public string theName;
+    public GameObject inputField;
+    public GameObject textDisplay;
+
+    public void StoreName()
+    {
+        theName = inputField.GetComponent<Text>().text;
+        textDisplay.GetComponent<Text>().text = "안녕하세요 " + theName + "님 게임을 플레이해주셔서 감사합니다.";
+    }
+
+
+
+}
